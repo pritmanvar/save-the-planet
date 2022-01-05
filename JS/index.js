2a698c43dc9c27f3cc0c87b14a014c0f2f270919
Prit Manvar <pritmanvar1109@gmail.com>
2022-01-06 01:26:23 +0530

    added feature to fire the bullet

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,13 @@
 let astroid = document.querySelector('.astroid');
 let img = document.querySelector('.astroid img');
-let audio = document.querySelector('audio');
+let blastSound = document.querySelector('.blastSound');
 
 // function to destroy the astroid.
 function destroy(clicked_id) {
     let astroid = document.getElementById(clicked_id);
     // play blast sound.
-    audio.play();
-    audio.volume = 0.02;
+    blastSound.cloneNode(true).play();
+    blastSound.volume = 0.1;
     astroid.classList.add('blast'); // add blast class into astroid
     setTimeout(() => { // remove that astroid after certain time period.
         astroid.remove();
--- JS/rotateWeapon.js
+++ JS/rotateWeapon.js
@@ -1,15 +1,25 @@
+let main = document.querySelector("main");
+let c = document.querySelector("canvas");
+let ctx = c.getContext("2d"); 
 let weapon = document.querySelector('.weapon');
+let fire = document.querySelector('.fire');
+
+// set height and width of canvas
+c.height = window.innerHeight;
+c.width = window.innerWidth;
 // coordinates of origin.
 const y1 = -c.height / 2;
 const x1 = c.width / 2;
+let angle; // angle of weapon
 
-c.addEventListener('mousemove',(e)=>{
+
+main.addEventListener('mousemove',(e)=>{
     // coordinates of mouse pointer.
     let y2 = -e.clientY; // taking with negative sign for correct quadrants system.
     let x2 = e.clientX;
 
     let m = (y2-y1)/(x2-x1); // slop of the line from (x2,y2) to (x1,y1)
-    let angle = Math.atan(m)*180/Math.PI; // angle of line
+    angle = Math.atan(m)*180/Math.PI; // angle of line
 
     // if current point is in 2nd or 3rd quadrant then i have to add 180deg into the angle.
     if(x1-x2 < 0){
@@ -17,4 +27,28 @@ c.addEventListener('mousemove',(e)=>{
     }
     // rotate weapon by that angle such that it moves corresponding to mouse
     weapon.style.transform = `translate(-50%,-50%) rotate(${-angle-90}deg)`;
-})
+});
+
+// it will generate and fire new bullet on every click
+main.addEventListener('click',()=>{
+    fire.cloneNode(true).play();
+    new generateBullet(angle);
+});
+
+// constructure to generate new bullete
+function generateBullet(angle){
+    // create a new span for bullete and add that into DOM.
+    this.bullet = document.createElement("SPAN");
+    main.appendChild(this.bullet);
+    // give bullete class and set it's position.
+    this.bullet.classList.add('bullet');
+    this.bullet.style.transform = `rotate(${-angle+180}deg)`;
+    this.bullet.style.left = x1 + "px";
+    this.bullet.style.top = -y1 + "px";
+    this.removeBullete = function () { // it will remove bullete from DOM.
+        setTimeout(() => {
+            this.bullet.remove();
+        }, 5000);
+    };
+    this.removeBullete();
+}
